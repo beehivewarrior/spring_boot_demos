276c9be972b4cbede550fd096531853df9dfd68a
Jordan Bigelow <jordan@jordanbigelow.com>
2021-10-23 13:29:06 -0600

    Committing Ldap Project

---
+++ ldap_authenticator/src/main/java/com/jordanbigelow/ldap_authenticator/LdapAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.jordanbigelow.ldap_authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapAuthenticatorApplication.class, args);
+    }
+
+}
---
+++ ldap_authenticator/src/test/java/com/jordanbigelow/ldap_authenticator/LdapAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordanbigelow.ldap_authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
